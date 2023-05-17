3f7aa8c469d8e32ca1afd6a2fed0dbefb3bebbbe
R. G. Esteves <rodolfo.g.esteves@intel.com>
2023-05-16 23:36:36 -0700

    `perf` command line (to be used via `perfcollect`)

--- measure_perf.sh
+++ measure_perf.sh
@@ -1,3 +1,6 @@
 #!/usr/bin/env bash
 
-dotnet-counters collect --process-id $PID 
+dotnet-counters collect --process-id $PID
+# Replicating the "one pager" methodology of Zhiguo Zhou et al.
+# This should be part of `perfcollect`
+# perf stat -x , -e task-clock,cycles,instructions,cycles:k,instructions:k,cycles:u,instructions:u -- dotnet run
